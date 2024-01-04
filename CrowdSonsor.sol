86ddf1dba3dfccf797a22d7d58cdeedce5305a21
tranchinh <chinhlong30032001@gmail.com>
2024-01-05 06:11:35 +0700

    update

--- CrowdSonsor.sol
+++ CrowdSonsor.sol
@@ -7,14 +7,17 @@ contract Crowdsponsor {
     enum TypePackage {Titanium, Gold, Silver}
     
     struct Event {
+        string eventId;
         string name;
         address[] participants;
         TypePackage category;
-        mapping (address => uint256) balanceAddress;
+        uint256 priceTitanium;
+        uint256 priceGold;
+        uint256 priceSilver;
     }
 
     mapping(address => string[]) public mapClientEvent;
-    mapping(string => Event) public DetailEvent;
+    mapping(string => Event) public detailEvent; // eventId -> Event
 
     event Deposit(address indexed participant, TypePackage category, uint256 amount);
 
@@ -22,26 +25,35 @@ contract Crowdsponsor {
         owner = msg.sender;
     }
 
-    function deposit(TypePackage _package, string memory name) external payable {
+    function createEvent (string memory _name, string memory _eventId, uint256 _priceTitanium, uint256 _priceGold, uint256 _priceSilver) public {
+            Event memory newEvent = Event({
+                eventId: _eventId,
+                name: _name,
+                participants: new address[](0),
+                priceTitanium: _priceTitanium,
+                priceGold: _priceGold,
+                priceSilver: _priceSilver,
+                category: TypePackage.Gold
+            });
+            mapClientEvent[msg.sender].push(_eventId);
+            detailEvent[_eventId] = newEvent;
+    }
+
+
+    function deposit(TypePackage _package, string memory _eventId, uint256 _price) external payable {
+        Event storage eventObject = detailEvent[_eventId];
         uint256 amount;
         if (_package == TypePackage.Titanium) {
-            amount = 500;
+            require(_price == eventObject.priceTitanium, "You deposit not enough price of Titanium Package");
         } else if (_package == TypePackage.Gold) {
-            amount = 300;
+            require(_price == eventObject.priceGold, "You deposit not enough price of Gold Package");
         } else if (_package == TypePackage.Silver) {
-            amount = 200;
+            require(_price == eventObject.priceTitanium, "You deposit not enough price of Silver Package");
         } else {
             revert("Invalid package type");
         }
-        require(msg.value == amount, "Incorrect deposit amount");
 
-         // Create a new event and assign values
-        Event storage newEvent = DetailEvent[name];
-        newEvent.name = name;
-        newEvent.participants.push(msg.sender);
-        newEvent.category = _package;
-        newEvent.balanceAddress[msg.sender] += amount;
-        mapClientEvent[msg.sender].push(name);
+        detailEvent[_eventId].participants.push(msg.sender);
 
         emit Deposit(msg.sender, _package, amount);
     }
